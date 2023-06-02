01568969ceed9d8d66c6dfc0562999c45c69dab4
kolsoum1381@gmail.com <kolsoum1381@gmail.com>
2023-06-03 00:10:57 +0330

    feat : create ServicesSection component

---
+++ components/page/home/ServicesSection.vue
@@ -0,0 +1,147 @@
+<template>
+  <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+        <div class="section-title">
+          <h2>Services</h2>
+          <p>
+            SocAI provides a wide range of services to hospitals and other
+            healthcare organizations.
+          </p>
+        </div>
+
+        <div class="row">
+          <div
+            class="col-xl-3 col-md-6 d-flex align-items-stretch"
+            data-aos="zoom-in"
+            data-aos-delay="100"
+          >
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4><a href="">Predictive modeling</a></h4>
+              <p>
+                Use statistical and machine learning techniques to forecast
+                future healthcare needs and trends, allowing for proactive and
+                targeted interventions
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0"
+            data-aos="zoom-in"
+            data-aos-delay="200"
+          >
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">Supply Chain Planning</a></h4>
+              <p>
+                Balance demand and supply while optimizing delivery procedures
+                by analyzing massive data sets and applying sophisticated
+                algorithms.
+              </p>
+            </div>
+          </div>       
+          <div
+            class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0"
+            data-aos="zoom-in"
+            data-aos-delay="300"
+          >
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4><a href="">Population health management</a></h4>
+              <p>
+                Help hospitals to identify and address the healthcare needs of
+                specific population groups, such as patients with chronic
+                conditions or those at risk of developing certain conditions.
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0"
+            data-aos="zoom-in"
+            data-aos-delay="400"
+          >
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-layer"></i></div>
+              <h4><a href="">Warehouse Analysis</a></h4>
+              <p>
+                Monitoring warehouse perimeter, automating barcode reading,
+                tracking employees and preventing thefts and violations.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div
+            class="col-xl-3 col-md-6 d-flex align-items-stretch"
+            data-aos="zoom-in"
+            data-aos-delay="500"
+          >
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4><a href="">Research support</a></h4>
+              <p>
+                Provide the data and insights needed to support research and
+                innovation in healthcare.
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0"
+            data-aos="zoom-in"
+            data-aos-delay="600"
+          >
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">Data visualization</a></h4>
+              <p>
+                Create visualizations of data, such as charts, graphs, and maps,
+                to make it easier to understand and interpret large amounts of
+                data.
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0"
+            data-aos="zoom-in"
+            data-aos-delay="700"
+          >
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4><a href="">Quality improvement</a></h4>
+              <p>
+                Identify opportunities for process improvement and cost
+                reduction, helping hospitals to deliver higher quality care more
+                efficiently.
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0"
+            data-aos="zoom-in"
+            data-aos-delay="800"
+          >
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-layer"></i></div>
+              <h4><a href="">Strategy and planning</a></h4>
+              <p>
+                Provide the data and insights needed to inform strategy and
+                planning decisions at the hospital and organizational levels.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'ServicesSection'
+}
+</script>
