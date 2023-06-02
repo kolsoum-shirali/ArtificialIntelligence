3bb127775f9f17db724e9b95b6419491ee1d0775
kolsoum1381@gmail.com <kolsoum1381@gmail.com>
2023-06-03 00:09:00 +0330

    feat : create AboutSection component

---
+++ components/page/home/AboutSection.vue
@@ -0,0 +1,48 @@
+<template>
+  <section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+      <div class="section-title">
+        <h2>Goals</h2>
+      </div>
+
+      <div class="row content">
+        <div class="col-lg-6">
+          <p>
+            Use data and analytics to improve the quality, safety, and
+            efficiency of patient care, enhance the patient experience, and
+            optimize hospital operations
+          </p>
+          <ul>
+            <li>
+              <i class="ri-check-double-line"></i> Protect the privacy and
+              security of health data
+            </li>
+            <li>
+              <i class="ri-check-double-line"></i> Collaborate with healthcare
+              professionals, researchers, and other stakeholders
+            </li>
+            <li>
+              <i class="ri-check-double-line"></i> Build a more efficient and
+              effective healthcare system
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-6 pt-4 pt-lg-0">
+          <p>
+            We are a group of data scientists, economists and optimization
+            experts determined to bring healthcare over to the bleeding edge of
+            operational efficiency powered by machine learning, data analytics
+            and deep neural networks.
+          </p>
+          <a href="#" class="btn-learn-more">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutSection',
+}
+</script>
