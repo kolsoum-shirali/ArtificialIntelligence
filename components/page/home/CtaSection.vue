b782c678be7bd5e58388d0c54f364bda7a9c366b
kolsoum1381@gmail.com <kolsoum1381@gmail.com>
2023-06-03 00:10:08 +0330

    feat : create CtaSection component

---
+++ components/page/home/CtaSection.vue
@@ -0,0 +1,23 @@
+<template>
+   <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="row">
+          <div class="col-lg-9 text-center text-lg-start">
+            <h3>Call To Action</h3>
+            <p> Don't miss this opportunity to make a real difference in the lives of your patients and in the healthcare system as a whole. Start using health analytics today and make a lasting impact on healthcare!</p>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="https://forms.gle/5BaMUZYqwfvmoXuFA">Book A Meeting</a>
+          </div>
+        </div>
+
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'CtaSection'
+}
+</script>
