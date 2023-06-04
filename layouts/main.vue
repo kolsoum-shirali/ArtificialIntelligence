bba71132fab5a10fe79c1940dbed44866de72773
kolsoum1381@gmail.com <kolsoum1381@gmail.com>
2023-06-04 11:04:38 +0330

    feat : create MainLayout

---
+++ layouts/main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div id="preloader"></div>
+    <AppHeader />
+    <div id="main">
+      <nuxt />
+    </div>
+    <AppFooter />
+    <a
+      href="#"
+      class="back-to-top d-flex align-items-center justify-content-center"
+      ><i class="bi bi-arrow-up-short"></i
+    ></a>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/layout/AppHeader.vue'
+import AppFooter from '@/components/layout/AppFooter.vue'
+export default {
+  name: 'MainLayout',
+  components: { AppHeader, AppFooter },
+}
+</script>
