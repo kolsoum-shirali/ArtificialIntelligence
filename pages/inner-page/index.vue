035c8f71054f98993a3f10dd896a0463b43415b0
kolsoum1381@gmail.com <kolsoum1381@gmail.com>
2023-06-04 11:05:24 +0330

    feat : create InnerPage

---
+++ pages/inner-page/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <BreadCrumbs title="Inner Page"/>
+    <InnerContent/>
+  </div>
+</template>
+
+<script>
+import BreadCrumbs from '@/components/common/BreadCrumbs.vue'
+import InnerContent from '@/components/page/innerPage/InnerContent.vue'
+export default {
+  name: 'InnerPage',
+  layout:'main',
+  components:{
+    BreadCrumbs,
+    InnerContent
+  },
+}
+</script>
