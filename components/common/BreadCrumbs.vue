0b167159c92208a61bad719cd2e92f76f18765cf
kolsoum1381@gmail.com <kolsoum1381@gmail.com>
2023-06-04 11:01:56 +0330

    feat : create BreadCrumbs

---
+++ components/common/BreadCrumbs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>{{ title }}</li>
+        </ol>
+        <h2>{{ title }}</h2>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BreadCrumbs',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
