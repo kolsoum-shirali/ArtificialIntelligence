3898b11bd195476d89d611795ea8890de6e470ef
kolsoum1381@gmail.com <kolsoum1381@gmail.com>
2023-06-04 11:03:38 +0330

    feat : create InnerContent

---
+++ components/page/innerPage/InnerContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <section class="inner-page">
+      <div class="container">
+        <p>
+          Example inner page template
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InnerContent'
+}
+</script>
