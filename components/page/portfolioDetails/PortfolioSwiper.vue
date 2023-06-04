692e6e8e331f92b288f3bd9ca921f814c75474cc
kolsoum1381@gmail.com <kolsoum1381@gmail.com>
2023-06-04 11:04:19 +0330

    feat : create PortfolioSwiper

---
+++ components/page/portfolioDetails/PortfolioSwiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="portfolio-details-slider swiper">
+    <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide v-for="(img,index) in images" :key="index">
+        <img
+          :src="require(`assets/images/portfolio/${img.title}`)"
+        />
+      </swiper-slide>
+      <div slot="pagination" class="swiper-pagination" />
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+import SwiperCore, { Pagination } from 'swiper/core'
+SwiperCore.use([Pagination])
+export default {
+  name: 'PortfolioSwiper',
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  props:{
+    images:{
+      type:Array,
+      default:()=>[]
+    }
+  },
+  data() {
+    return {
+      swiperOptions: {
+        loop: true,
+        slidesPerView: 'auto',
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      },
+    }
+  },
+}
+</script>
